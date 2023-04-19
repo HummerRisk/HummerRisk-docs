1938d8f607f81746c71bcf88acd17812e43be15e
Alvin <alvin@hummercloud.com>
2023-04-19 17:24:54 +0800

    Deployed 536d311 to v0.10 with MkDocs 1.4.2 and mike 1.1.2

---
+++ v0.10/js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e796d74d5712b3a011fbb22f56d58959";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
